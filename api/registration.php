31925353385e23e3625932a03ad9514aadc53a61
Revanth <revanthapollo@gmail.com>
2021-11-23 23:33:43 +0530

    registration page update

--- api/registration.php
+++ api/registration.php
@@ -8,19 +8,9 @@ $data = json_decode(file_get_contents('php://input'), true);
 $name = $data['name'];
 $mobile_number = $data['mobile_number'];
 $password = $data['password'];
-$referral_mobile_number = $data['referral_mobile_number'];
-$book_id = $data['book_id'];
-	
+$email = $data['email'];
 
-//referral_mobile_number_data
-$referral_mobile_number_data = $dbConn->query("SELECT
-    *
- FROM user
- WHERE mobile_number IN ('$referral_mobile_number')
-    ");
-while($row = $referral_mobile_number_data->fetch(PDO::FETCH_ASSOC)) {
-    $referral_id = $row['user_id'];
-}
+
 
 //duplicate_mobile_number
 $duplicate_mobile_number = $dbConn->query("SELECT
@@ -34,7 +24,8 @@ while($row = $duplicate_mobile_number->fetch(PDO::FETCH_ASSOC)) {
 
 
 //error_message
-if ($referral_id == "" || $duplicate_data == "1" || $book_id == "") {
+if (isset($duplicate_data) || !isset($name) || !isset($mobile_number)
+    || !isset($password) || !isset($email)) {
   $returnArr = array("api"=>"registration","result"=>"error");
 }
 
@@ -45,32 +36,19 @@ else{
   $dbConn->query("INSERT INTO `user` (
     `name`,
     `mobile_number`,
-    `referral_id`,
+    `email`,
     `password`
     ) 
     VALUES (
     '{$name}',
     '{$date}',
-    '{$referral_id}',
+    '{$email}',
     '{$password}'
     )
     ;");
 
-  $user_id = $dbConn->lastInsertId();
 
-//inserting_payment_data
-  $dbConn->query("INSERT INTO `payment` (
-    `user_id`,
-    `book_id`
-    ) 
-    VALUES (
-    '{$user_id}',
-    '{$book_id}'
-    )
-    ;");
-
-
-	$returnArr = array("api"=>"registration","result"=>"success");
+    $returnArr = array("api"=>"registration","result"=>"success");
 }
 
 
