eb2ce7d2bd1cdce49c4662d1bde54b124213b231
Revanth <revanthapollo@gmail.com>
2021-11-27 21:17:14 +0530

    book_view and sql

--- kubera.sql
+++ kubera.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2021 at 10:42 AM
+-- Generation Time: Nov 27, 2021 at 04:46 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -29,16 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `author` (
   `author_id` int(11) NOT NULL,
-  `author_name` varchar(255) NOT NULL
+  `author_name` varchar(255) NOT NULL,
+  `author_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `author`
 --
 
-INSERT INTO `author` (`author_id`, `author_name`) VALUES
-(1, 'Nolan'),
-(2, 'Rowling');
+INSERT INTO `author` (`author_id`, `author_name`, `author_image`) VALUES
+(1, 'Nolan', 'https://www.hollywoodreporter.com/wp-content/uploads/2021/09/Christopher-Nolan-attends-the-screening-of-2001-A-Space-Odyssey-Getty-H-2021.jpg'),
+(2, 'Rowling', 'https://www.globaltimes.cn/Portals/0/attachment/2020/2020-11-15/5aa0f4cd-ca32-46aa-ac18-79a047db4ebe.jpeg');
 
 -- --------------------------------------------------------
 
@@ -49,6 +50,7 @@ INSERT INTO `author` (`author_id`, `author_name`) VALUES
 CREATE TABLE `book` (
   `book_id` int(11) NOT NULL,
   `book_name` varchar(255) NOT NULL,
+  `book_image` varchar(255) NOT NULL,
   `amount` varchar(255) NOT NULL,
   `book_path` varchar(255) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 0,
@@ -61,9 +63,9 @@ CREATE TABLE `book` (
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`book_id`, `book_name`, `amount`, `book_path`, `status`, `trending`, `category_id`, `author_id`) VALUES
-(1, 'Memento short story', '100', 'https://www.londonscreenwritersfestival.com/assets/Memento-Short-Story-by-Jonathan-Nolan.pdf', 0, 0, 1, 1),
-(2, 'Harry Potter Book 1', '150', 'https://hpread.scholastic.com/HP_Book1_Chapter_Excerpt.pdf', 0, 1, 2, 2);
+INSERT INTO `book` (`book_id`, `book_name`, `book_image`, `amount`, `book_path`, `status`, `trending`, `category_id`, `author_id`) VALUES
+(1, 'Memento short story', 'https://nofilmschool.com/sites/default/files/uploads/2013/06/polaroid.jpg', '100', 'https://www.londonscreenwritersfestival.com/assets/Memento-Short-Story-by-Jonathan-Nolan.pdf', 0, 0, 1, 1),
+(2, 'Harry Potter Book 1', 'https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg', '150', 'https://hpread.scholastic.com/HP_Book1_Chapter_Excerpt.pdf', 0, 1, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -73,16 +75,17 @@ INSERT INTO `book` (`book_id`, `book_name`, `amount`, `book_path`, `status`, `tr
 
 CREATE TABLE `category` (
   `category_id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL
+  `category_name` varchar(255) NOT NULL,
+  `category_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`category_id`, `category_name`) VALUES
-(1, 'Fiction'),
-(2, 'Entertainment');
+INSERT INTO `category` (`category_id`, `category_name`, `category_image`) VALUES
+(1, 'Fiction', 'https://www.indianfolk.com/wp-content/uploads/2017/09/booklights.jpg'),
+(2, 'Entertainment', 'https://www.popoptiq.com/wp-content/uploads/2019/01/1-26-1-870x653.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,6 +118,13 @@ CREATE TABLE `user` (
   `level` int(11) NOT NULL DEFAULT 0,
   `stage` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `name`, `mobile_number`, `referral_id`, `password`, `registered_date`, `status`, `referral_count`, `level`, `stage`) VALUES
+(1, 'admin', '12345678', 0, '0', '2021-11-27 21:13:46', 0, 0, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -182,7 +192,7 @@ ALTER TABLE `payment`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
