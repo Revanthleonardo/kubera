1050d7cdb4aa61f68e41a36509ead63541a1cb64
Revanth <revanthapollo@gmail.com>
2021-12-12 19:23:43 +0530

    less than 4 referral count

--- api/test.php
+++ api/test.php
@@ -2,21 +2,24 @@
 
 include '../config.php';
 
-if(isset($_POST['referral_number_input'])) { 
-$user_id = $_POST['user_id'];
-$referral_number = $_POST['referral_number'];
 
-echo $user_id;
+$user_id = $_GET['user_id'];
+$referral_number = $_GET['referral_number'];
+
+echo "user_id = ".$user_id;
 echo "<br>";
-echo $referral_number;
+echo "referral_number = ".$referral_number;
+echo "<br>";
 echo "<br>";
 
 //user_payment_check
+//and status whether referral_number updated
 $user_payment_check = $dbConn->query("SELECT
     *
  FROM user
  WHERE user_id IN ('$user_id')
  AND payment_status IN ('$inactive')
+ AND status IN ('$active')
     ");
 
 while($row = $user_payment_check->fetch(PDO::FETCH_ASSOC)) {
@@ -37,10 +40,22 @@ $referral_number_user_data = $dbConn->query("SELECT
 
 while($row = $referral_number_user_data->fetch(PDO::FETCH_ASSOC)) {
     $referral_number_user_data_referral_count = $row['referral_count'];
+    $referral_number_user_data_user_id = $row['user_id'];
 
     //if less than 4 insert and update count
     if ($referral_number_user_data_referral_count < 4) {
-    	echo "insert data and update count";
+    	//
+    	echo "less than 4 insert and update count";
+    	
+    	//update status for user
+    	$dbConn->query("UPDATE `user` SET `status` = '$inactive',
+    	`referral_id` = '$referral_number_user_data_user_id' WHERE user_id IN ('$user_id')");
+
+    	//add 1 to referral count to user
+    	$actual_referral_number_user_data_referral_count = $referral_number_user_data_referral_count + 1;
+
+    	//update referral count for referrer
+    	$dbConn->query("UPDATE `user` SET `referral_count` = '$actual_referral_number_user_data_referral_count' WHERE referral_number IN ('$referral_number')");
     }
 
 }
@@ -52,24 +67,6 @@ else{
 }
 
 
-}
+
 
 ?>
-
-<form action="" method="POST">
-	<table>
-		<tr>
-		<th>User ID</th>
-		<td><input type="number" name="user_id" min="6" max="1000"></td>
-		</tr>
-		<tr>
-		<th>Referral number</th>
-		<td><input type="number" name="referral_number" min="1" max="1000" value="1"></td>
-		</tr>
-		<tr>
-			<td><input type="submit" name="referral_number_input" value="submit"></td>
-		</tr>
-	</table>
-	
-	
-</form>
