5745f6d0dc6ff57ba3ae4564c958d6bac44f7a08
Revanth <revanthapollo@gmail.com>
2021-12-22 00:57:58 +0530

    level upgrade

--- api/level.php
+++ api/level.php
@@ -2,27 +2,77 @@
 
 include '../config.php';
 
-//level_data
-$level_user_id_input = $_GET['user_id'];
 
-while ($level_user_id_input >= 1) {
+//user_list
+$user_list = $dbConn->query("SELECT
+    *
+ FROM user
+ WHERE user_id IN ('6')
 
-$level_get_level_data = $dbConn->query("SELECT 
+    ");
+while($row = $user_list->fetch(PDO::FETCH_ASSOC)) {
+    $level_user_id_input = $row['user_id'];
+
+//stage_1
+$stage_1_data = $dbConn->query("SELECT 
 *
   FROM user   
-  WHERE user_id IN ('$level_user_id_input')
-  LIMIT 1
+  WHERE referral_id IN ('$level_user_id_input')
+  
 ");
-if($row = $level_get_level_data->fetch(PDO::FETCH_ASSOC)) {
+while($row = $stage_1_data->fetch(PDO::FETCH_ASSOC)) {
 
-$level_user_id = $row['user_id'];
-$level_referral_id = $row['referral_id'];
+$stage_1_user_id[] = $row['user_id'];
+}
+//stage_1
 
-$level_user_id_input = $level_referral_id;
+//stage_2
+foreach ($stage_1_user_id as $key => $stage_1_user_id_value) {
+$stage_2_data = $dbConn->query("SELECT 
+*
+  FROM user   
+  WHERE referral_id IN ('$stage_1_user_id_value')
+  
+");
+while($row = $stage_2_data->fetch(PDO::FETCH_ASSOC)) {
 
-echo $level_user_id;
+$stage_2_user_id[] = $row['user_id'];
+}
+}
+//stage_2
+
+//stage_3
+foreach ($stage_2_user_id as $key => $stage_2_user_id_value) {
+$stage_3_data = $dbConn->query("SELECT 
+*
+  FROM user   
+  WHERE referral_id IN ('$stage_2_user_id_value')
+  
+");
+while($row = $stage_3_data->fetch(PDO::FETCH_ASSOC)) {
+
+$stage_3_user_id[] = $row['user_id'];
+}
+}
+
+//stage_3
+echo json_encode($stage_1_user_id);
+echo json_encode($stage_2_user_id);
+echo json_encode($stage_3_user_id);
+
+
+$total_count =  count($stage_1_user_id) + count($stage_2_user_id) + count($stage_3_user_id);
+
 echo "<br>";
+
+echo $total_count;
 }
-}
+
+
+
+
+
+
+
 
 ?>
---
+++ api/level_test_upgrade.php
@@ -0,0 +1,18 @@
+<?php
+
+
+include '../config.php';
+
+
+
+$referral_number = "1";
+
+for ($i="6"; $i < ; $i++) { 
+
+	$user_id = $i;
+	
+
+	include "test.php";
+}
+
+?>
