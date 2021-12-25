e45064d6e6b36aa9ce44b3007f118762418fec95
Revanth <revanthapollo@gmail.com>
2021-12-25 23:14:07 +0530

    view_tree update

--- api/view_tree.php
+++ api/view_tree.php
@@ -4,28 +4,21 @@ include '../config.php';
 
 
 $user_id = $_GET['user_id'];
-$level = $_GET['level'];
 
-
+if (isset($user_id)){
 //view_tree_user_data
 //and status whether referral_number updated
 $view_tree_user_data = $dbConn->query("SELECT
+    user_id,
     name,
-    mobile_number,
-    email,
-    registered_date,
-    referral_number,
-    payment_status,
-    referral_count,
-    stage,
-    level
+    referral_number
  FROM user
  WHERE referral_id IN ('$user_id')
- AND level IN ('$level')
     ");
 
 while($row = $view_tree_user_data->fetch(PDO::FETCH_ASSOC)) {
     $view_tree_api[] = $row;
+}
 }
 
 if (isset($view_tree_api)) {
