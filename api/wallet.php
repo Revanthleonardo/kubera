36e9a17f21fc67a58ae84cb25d8987ffa68b0a91
Revanth <revanthapollo@gmail.com>
2022-01-03 12:42:00 +0530

    wallets

--- api/wallet.php
+++ api/wallet.php
@@ -6,15 +6,42 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 $user_id = $data['user_id'];
 
+//wallet_level_data
+$wallet_level_data = $dbConn->query("SELECT
+    level
+ FROM user
+ WHERE user_id IN ('$user_id')
+    ");
+
+while($row = $wallet_level_data->fetch(PDO::FETCH_ASSOC)) {
+    $level = $row['level'];
+
+    //level 2 - 4
+    if ($level == "2") {
+        $actual_level = "1";
+    }
+
+    if ($level == "3") {
+        $actual_level = "2";
+    }
+
+    if ($level == "4") {
+        $actual_level = "3";
+    }
+
+    if ($level == "1") {
+        $actual_level = "0";
+    }
+
+
+}
+
+
 //wallet
 $wallet = $dbConn->query("SELECT
-    wallet.level,
     wallet.reward
- FROM user
-  LEFT JOIN 
-  wallet ON
-  user.level = wallet.level
- WHERE user.user_id IN ('$user_id')
+ FROM wallet
+ WHERE level IN ('$actual_level')
     ");
 
 while($row = $wallet->fetch(PDO::FETCH_ASSOC)) {
